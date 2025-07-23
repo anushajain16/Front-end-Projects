ad7b09581bf7a0e7aa8c1050c2e6d12aafb43aa8
anushajain16 <anusha.j@ahduni.edu.in>
2025-07-23 20:01:25 +0530

    Changes

--- Portfolio website/portfolio.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("resume").addEventListener("click", function () {
-    window.open("https://drive.google.com/file/d/1d6dNp1RlTfA3ISfBMpBz6ZRUQ3LnJDaM/view?usp=sharing", "_blank");
-});
-
-document.getElementById("account").addEventListener("click", function () {
-    window.open("https://www.linkedin.com/in/anusha-jain-99681a2a6/", "_blank");
-});
-
-
